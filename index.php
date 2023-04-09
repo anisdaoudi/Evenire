535308e75320dd4e49fe025f2e381f366abd7016
anisdaoudi <123045911+anisdaoudi@users.noreply.github.com>
2023-04-10 01:20:18 +0200

    footer

--- index.php
+++ index.php
@@ -176,8 +176,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 
-<footer>
-    <nav class="nav flex-row">
+<footer class="footer">
+    <nav class="footer-nav">
         <a class="nav-link" href="#">Accueil</a>
         <a class="nav-link" href="#">Evenements</a>
         <a class="nav-link" href="#">Profil</a>
