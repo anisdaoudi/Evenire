45ed4ad89f14deb7176954a795d5aad8f0775480
Lyes <lyes.dabla@gmail.com>
2023-04-11 00:12:28 +0200

    adding first and last name

--- controller/signup.php
+++ controller/signup.php
@@ -2,20 +2,30 @@
 require('../pdo.php');
 
 
-$email = $_POST['email'];
-$username = $_POST['username'];
-$password = $_POST['password'];
+$email     = $_POST['email'];
+$username  = $_POST['username'];
+$password  = $_POST['password'];
 $birthdate = $_POST['birth'];
-$username = $_POST['username'];
+$firstname = $_POST['firstname'];
+$lastname  = $_POST['lastname'];
+$username  = $_POST['username'];
 
 // Vérifier si l'e-mail existe
 $query = $pdo->prepare( "select * from compte where email = ?;");
 $query->execute([$email]);
 
 $emailArray = $query->fetchAll();
-
 if(count($emailArray) > 0){
     header("location:../inscription1.php?error=emailExists");  
+    exit();
+} 
+
+// verifier si le pseudo est unique
+
+$query = $pdo->prepare( "select * from compte where pseudo = ?;");
+$pseudoArray = $query->fetchAll();
+if(count($pseudoArray) > 0){
+    header("location:../inscription1.php?error=usernameExists");  
     exit();
 } 
 
@@ -23,8 +33,8 @@ if(count($emailArray) > 0){
 $password = hash('sha256',$password);
 
 // Insertion des données
-$query = $pdo->prepare( "INSERT INTO compte (abonnements,nb_abonnés,date_de_naissance,mdp,certification,email,pseudo)
-                         VALUES (0,0,?,?,0,?,?)");
-$query->execute([$birthdate,$password,$email,$username]);
+$query = $pdo->prepare( "INSERT INTO compte (abonnements,nb_abonnés,date_de_naissance,mdp,certification,email,pseudo,nom,prenom)
+                         VALUES (0,0,?,?,0,?,?,?,?)");
+$query->execute([$birthdate,$password,$email,$username,$lastname,$firstname]);
 
 header("location:../connexion.php");  
